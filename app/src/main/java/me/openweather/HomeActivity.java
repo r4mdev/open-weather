93be26e2719027e2b0714e0b246ee5717c9101af
Hanifan Azka <hanifan.azka@gmail.com>
2023-05-16 18:30:09 +0530

    feat: Add spinner dropdown

--- app/src/main/java/me/openweather/HomeActivity.java
+++ app/src/main/java/me/openweather/HomeActivity.java
@@ -1,6 +1,8 @@
 package me.openweather;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import me.openweather.*;
@@ -13,6 +15,15 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.city_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
         initialLogin();
     }
 
