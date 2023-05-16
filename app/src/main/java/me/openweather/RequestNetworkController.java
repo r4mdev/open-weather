86e0b0b62a9cd05273150a96079857247c6c31fd
ram <janakirammalladi10@gmail.com>
2023-05-16 16:50:20 +0530

    Added more layouts and java files

--- app/src/main/java/me/openweather/RequestNetworkController.java
+++ app/src/main/java/me/openweather/RequestNetworkController.java
@@ -30,7 +30,7 @@ import okhttp3.Response;
 public class RequestNetworkController {
     public static final String GET = "GET";
     public static final String POST = "POST";
-    public static fi nal String PUT = "PUT";
+    public static final String PUT = "PUT";
     public static final String DELETE = "DELETE";
 
     public static final int REQUEST_PARAM = 0;
--- app/src/main/java/me/openweather/SettingsPage.java
+++ app/src/main/java/me/openweather/SettingsPage.java
@@ -1,20 +1,16 @@
 package me.openweather;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
-import me.openweather.*;
-import me.openweather.R;
 
 @SuppressLint("CustomSplashScreen")
-public class GetStarted extends AppCompatActivity {
+public class SettingsPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.get_start);
+        setContentView(R.layout.settings_page);
 
     }
 }
